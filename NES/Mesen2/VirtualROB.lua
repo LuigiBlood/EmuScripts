9ecbe7d133498f20c1c81c1537ce534d7ea20eec
LuigiBlood <luigiblood@gmail.com>
2024-03-27 16:56:27 +0100

    [VirtualROB] Refactor variables into tables

--- NES/Mesen2/VirtualROB.lua
+++ NES/Mesen2/VirtualROB.lua
@@ -1,4 +1,4 @@
-﻿-- Virtual Robotic Operating Buddy
+﻿-- Virtual Robotic Operating Buddy (Mesen 2.0)
 -- Code by LuigiBlood
 frameCount = 0
 command = 0
@@ -20,31 +20,35 @@ rob = {
 	arms_max = 2
 }
 
+phys = {
+	accel = 0.1,
+	accel_max = 1,
+
+	y_base = 6.0,
+	y_max = 7.5
+}
+
+hud = {
+	x = 200,
+	y = 172,
+	x_scale = 10
+}
+
+config = {
+	--amount of frames for a gyro to spin
+	spin_max = 60*10
+}
+
 objects = {}
 
-accel = 0.1
-accel_max = 1
-blue_press = 0
-red_press = 0
-
-y_base = 6.0
-y_max = 7.5
-x_scale = 10
-
-hud_x = 200
-hud_y = 172
-
---amount of frames for a gyro to spin
-spin_max = 60*10
-
-prev_buffer = {}
+prevScreenBuffer = {}
 
 --Utility
 function GetMousePositionHUD()
 	local mouse = emu.getMouseState()
 	local ret = {}
-	ret.x = (mouse.x - hud_x) / x_scale
-	ret.y = (mouse.y - hud_y) / 8
+	ret.x = (mouse.x - hud.x) / hud.x_scale
+	ret.y = (mouse.y - hud.y) / 8
 	ret.left = mouse.left
 
 	return ret
@@ -149,8 +153,8 @@ function HandlePhysicsObject(self)
 
 	--Handle Physics
 	local lowobj = FindBelowObject(self.x, self.y)
-	if (lowobj == nil) then lowobj = { y = y_max } end
-	if lowobj.y > y_base and self.x ~= math.floor(self.x) then lowobj = {y = y_max} end
+	if (lowobj == nil) then lowobj = { y = phys.y_max } end
+	if lowobj.y > phys.y_base and self.x ~= math.floor(self.x) then lowobj = {y = phys.y_max} end
 
 	if self.grabbed == 1 then
 		local upobj = FindAboveObject(self.x, self.y)
@@ -162,10 +166,10 @@ function HandlePhysicsObject(self)
 		self.y = rob.y
 		self.gravity = 0
 	elseif self.y < lowobj.y - 1 then
-		if self.gravity < accel_max then
-			self.gravity = self.gravity + accel
+		if self.gravity < phys.accel_max then
+			self.gravity = self.gravity + phys.accel
 		else
-			self.gravity = accel_max
+			self.gravity = phys.accel_max
 		end
 		self.y = self.y + self.gravity
 	else
@@ -230,13 +234,13 @@ function StartCommand(command)
 	elseif currentCommand == 0010 then
 		--Down + 1
 		rob.ynext = math.floor(rob.y + 1 + 0.5)
-		if rob.ynext > y_base - 1 then rob.ynext = y_base - 1 end
+		if rob.ynext > phys.y_base - 1 then rob.ynext = phys.y_base - 1 end
 		rob.state = 4
 		rob.led = 1
 	elseif currentCommand == 1101 then
 		--Down + 2
 		rob.ynext = math.floor(rob.y + 2 + 0.5)
-		if rob.ynext > y_base - 1 then rob.ynext = y_base - 1 end
+		if rob.ynext > phys.y_base - 1 then rob.ynext = phys.y_base - 1 end
 		rob.state = 4
 		rob.led = 1
 	elseif currentCommand == 1010 then
@@ -315,8 +319,8 @@ function HandleROBState()
 end
 
 function DrawROB(x, y)
-	emu.drawRectangle(x - 5, y - 4, x_scale*5 + 8, 8*(y_max+1) + 2, 0x003F3F00, 0)
-	emu.drawRectangle(x - 4, y - 3, x_scale*5 + 6, 8*(y_max+1), 0x3F1F1F00, 1)
+	emu.drawRectangle(x - 5, y - 4, hud.x_scale*5 + 8, 8*(phys.y_max+1) + 2, 0x003F3F00, 0)
+	emu.drawRectangle(x - 4, y - 3, hud.x_scale*5 + 6, 8*(phys.y_max+1), 0x3F1F1F00, 1)
 
 	--ROB face
 	--emu.drawRectangle(x + 10, y + 3, 20, 10, 0x3FFFFFFF, 0)
@@ -324,18 +328,18 @@ function DrawROB(x, y)
 	--emu.drawRectangle(x + 22, y + 5, 6, 6, 0x3FFFFFFF, 0)
 	--emu.drawRectangle(x + 17, y + 13, 6, 20, 0x3FFFFFFF, 0)
 	
-	emu.drawRectangle(x + 1 + 2 * x_scale, y - 3, 6, 3, 0x3FFF7F7F, rob.led)
+	emu.drawRectangle(x + 1 + 2 * hud.x_scale, y - 3, 6, 3, 0x3FFF7F7F, rob.led)
 
 	--center of arms
-	emu.drawPixel(x + 3 + (rob.x * x_scale), y + 3 + (rob.y * 8), 0x3F7FFF7F)
-	emu.drawPixel(x + 4 + (rob.x * x_scale), y + 4 + (rob.y * 8), 0x3F7FFF7F)
+	emu.drawPixel(x + 3 + (rob.x * hud.x_scale), y + 3 + (rob.y * 8), 0x3F7FFF7F)
+	emu.drawPixel(x + 4 + (rob.x * hud.x_scale), y + 4 + (rob.y * 8), 0x3F7FFF7F)
 	--emu.drawRectangle(x + (rob.x * 8), y + (rob.y * 8), 8, 8, 0x3F7FFF7F, 0)
 	
 	DrawObjects(x, y)
 
 	--arms
-	emu.drawRectangle(x + 0 + (rob.x * x_scale) - rob.arms, y + (rob.y * 8), 4, 8, 0x3F7FFF7F, 0)
-	emu.drawRectangle(x + 4 + (rob.x * x_scale) + rob.arms, y + (rob.y * 8), 4, 8, 0x3F7FFF7F, 0)
+	emu.drawRectangle(x + 0 + (rob.x * hud.x_scale) - rob.arms, y + (rob.y * 8), 4, 8, 0x3F7FFF7F, 0)
+	emu.drawRectangle(x + 4 + (rob.x * hud.x_scale) + rob.arms, y + (rob.y * 8), 4, 8, 0x3F7FFF7F, 0)
 end
 
 function CheckScreen()
@@ -412,9 +416,9 @@ function updateROB()
 
 	--Anti-Flashing
 	if frameCount > 0 or (frameCount == 0 and color == 0) then
-		emu.setScreenBuffer(prev_buffer)
+		emu.setScreenBuffer(prevScreenBuffer)
 	elseif color == -1 then
-		prev_buffer = emu.getScreenBuffer()
+		prevScreenBuffer = emu.getScreenBuffer()
 	end
 
 	--Display
@@ -434,7 +438,7 @@ function updateROB()
 	--emu.drawString(10, 12*2, "Gyro2 X: " .. objects.gyro2.x, 0xFFFFFF, 0xFF000000)
 	--emu.drawString(10, 12*3, "Gyro2 Y: " .. objects.gyro2.y, 0xFFFFFF, 0xFF000000)
 
-	DrawROB(hud_x, hud_y)
+	DrawROB(hud.x, hud.y)
 	DrawMouseDebug()
 end
 
@@ -457,47 +461,47 @@ function DrawGyroObject(self, x, y)
 	local color = self.color
 	if self.grabbed == 1 or self.mousegrab == 1 then color = self.colorgrab end
 
-	if self.y <= y_base then
-		emu.drawRectangle(x + 0 + (self.x * x_scale), y + 1 + (self.y * 8), 8, 2, color, 1)
-		emu.drawRectangle(x + 1 + (self.x * x_scale), y + 3 + (self.y * 8), 6, 2, color, 1)
-		emu.drawRectangle(x + 2 + (self.x * x_scale), y + 5 + (self.y * 8), 4, 2, color, 1)
-		emu.drawRectangle(x + 3 + (self.x * x_scale), y + 7 + (self.y * 8), 2, 2, color, 1)
+	if self.y <= phys.y_base then
+		emu.drawRectangle(x + 0 + (self.x * hud.x_scale), y + 1 + (self.y * 8), 8, 2, color, 1)
+		emu.drawRectangle(x + 1 + (self.x * hud.x_scale), y + 3 + (self.y * 8), 6, 2, color, 1)
+		emu.drawRectangle(x + 2 + (self.x * hud.x_scale), y + 5 + (self.y * 8), 4, 2, color, 1)
+		emu.drawRectangle(x + 3 + (self.x * hud.x_scale), y + 7 + (self.y * 8), 2, 2, color, 1)
 	else
-		emu.drawRectangle(x + 1 + (self.x * x_scale), y + 0 + (self.y * 8), 2, 8, color, 1)
-		emu.drawRectangle(x + 3 + (self.x * x_scale), y + 1 + (self.y * 8), 2, 6, color, 1)
-		emu.drawRectangle(x + 5 + (self.x * x_scale), y + 2 + (self.y * 8), 2, 4, color, 1)
-		emu.drawRectangle(x + 7 + (self.x * x_scale), y + 3 + (self.y * 8), 2, 2, color, 1)
+		emu.drawRectangle(x + 1 + (self.x * hud.x_scale), y + 0 + (self.y * 8), 2, 8, color, 1)
+		emu.drawRectangle(x + 3 + (self.x * hud.x_scale), y + 1 + (self.y * 8), 2, 6, color, 1)
+		emu.drawRectangle(x + 5 + (self.x * hud.x_scale), y + 2 + (self.y * 8), 2, 4, color, 1)
+		emu.drawRectangle(x + 7 + (self.x * hud.x_scale), y + 3 + (self.y * 8), 2, 2, color, 1)
 	end
 
 	if self.spin > 0 then
 		local frames = 7 - emu.getState().frameCount % 8
 		if (self.spin <= 60*5) then frames = 8 - emu.getState().frameCount % 16 / 2 end
 		if (self.spin <= 60*2) then frames = 8 - emu.getState().frameCount % 32 / 4 end
-		emu.drawPixel(x + 0 + (self.x * x_scale) + frames, y + 0 + (self.y * 8), 0x00FFFFFF)
+		emu.drawPixel(x + 0 + (self.x * hud.x_scale) + frames, y + 0 + (self.y * 8), 0x00FFFFFF)
 	end
 end
 
 function HandleGyroObject(self)
 	HandlePhysicsObject(self)
 	if self.spin > 0 then self.spin = self.spin - 1 end
-	if self.y > y_base then self.spin = 0 end
+	if self.y > phys.y_base then self.spin = 0 end
 	if objects.bluebtn.x == self.x or objects.redbtn.x == self.x then
-		if self.grabbed == 0 and self.spin <= 0 and self.y == y_base - 1 then self.x = self.x - 0.2 end
+		if self.grabbed == 0 and self.spin <= 0 and self.y == phys.y_base - 1 then self.x = self.x - 0.2 end
 	end
 end
 
 function DrawGyroMotorObject(self, x, y)
-	emu.drawRectangle(x + 2 + (self.x * x_scale), y + 0 + (self.y * 8), 4, 2, self.color, 1)
-	emu.drawRectangle(x + 1 + (self.x * x_scale), y + 2 + (self.y * 8), 6, 2, self.color, 1)
+	emu.drawRectangle(x + 2 + (self.x * hud.x_scale), y + 0 + (self.y * 8), 4, 2, self.color, 1)
+	emu.drawRectangle(x + 1 + (self.x * hud.x_scale), y + 2 + (self.y * 8), 6, 2, self.color, 1)
 end
 
 function HandleGyroMotorObject(self)
-	if objects.gyro1.x == self.x and objects.gyro1.y == (self.y - 1) then objects.gyro1.spin = spin_max end
-	if objects.gyro2.x == self.x and objects.gyro2.y == (self.y - 1) then objects.gyro2.spin = spin_max end
+	if objects.gyro1.x == self.x and objects.gyro1.y == (self.y - 1) then objects.gyro1.spin = config.spin_max end
+	if objects.gyro2.x == self.x and objects.gyro2.y == (self.y - 1) then objects.gyro2.spin = config.spin_max end
 end
 
 function DrawGyroButtonObject(self, x, y)
-	emu.drawRectangle(x + 1 + (self.x * x_scale), y + 0 + (self.y * 8) + self.pressed, 6, 2, self.color, 1)
+	emu.drawRectangle(x + 1 + (self.x * hud.x_scale), y + 0 + (self.y * 8) + self.pressed, 6, 2, self.color, 1)
 end
 
 function HandleGyroButtonObject(self)
@@ -509,7 +513,7 @@ function StartRobotGyro()
 	objects = {}
 	objects.gyro1 = {
 		x = 0,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -524,7 +528,7 @@ function StartRobotGyro()
 	
 	objects.gyro2 = {
 		x = 1,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -539,7 +543,7 @@ function StartRobotGyro()
 	
 	objects.spinner = {
 		x = 4,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -553,7 +557,7 @@ function StartRobotGyro()
 	
 	objects.bluebtn = {
 		x = 2,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -568,7 +572,7 @@ function StartRobotGyro()
 	
 	objects.redbtn = {
 		x = 3,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -583,7 +587,7 @@ function StartRobotGyro()
 
 	objects.holder1 = {
 		x = 0,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -597,7 +601,7 @@ function StartRobotGyro()
 
 	objects.holder2 = {
 		x = 1,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -615,21 +619,21 @@ end
 function DrawBlockObject(self, x, y)
 	local color = self.color
 	if self.grabbed == 1 or self.mousegrab == 1 then color = self.colorgrab end
-	emu.drawRectangle(x + (self.x * x_scale), y + (self.y * 8), 8, 8, color, 1)
+	emu.drawRectangle(x + (self.x * hud.x_scale), y + (self.y * 8), 8, 8, color, 1)
 	--highlight
-	emu.drawLine(x + (self.x * x_scale), y + (self.y * 8), x + (self.x * x_scale) + 7, y + (self.y * 8), self.colorgrab)
-	emu.drawLine(x + (self.x * x_scale), y + (self.y * 8), x + (self.x * x_scale), y + (self.y * 8) + 7, self.colorgrab)
+	emu.drawLine(x + (self.x * hud.x_scale), y + (self.y * 8), x + (self.x * hud.x_scale) + 7, y + (self.y * 8), self.colorgrab)
+	emu.drawLine(x + (self.x * hud.x_scale), y + (self.y * 8), x + (self.x * hud.x_scale), y + (self.y * 8) + 7, self.colorgrab)
 	--shadow
-	emu.drawLine(x + (self.x * x_scale) + 1, y + (self.y * 8) + 7, x + (self.x * x_scale) + 7, y + (self.y * 8) + 7, 0xAF000000)
-	emu.drawLine(x + (self.x * x_scale) + 7, y + (self.y * 8) + 1, x + (self.x * x_scale) + 7, y + (self.y * 8) + 7, 0xAF000000)
+	emu.drawLine(x + (self.x * hud.x_scale) + 1, y + (self.y * 8) + 7, x + (self.x * hud.x_scale) + 7, y + (self.y * 8) + 7, 0xAF000000)
+	emu.drawLine(x + (self.x * hud.x_scale) + 7, y + (self.y * 8) + 1, x + (self.x * hud.x_scale) + 7, y + (self.y * 8) + 7, 0xAF000000)
 
 end
 
 function DrawHolderObject(self, x, y)
-	emu.drawRectangle(x + 1 + (self.x * x_scale), y + (self.y * 8), 6, 4, self.color, 1)
+	emu.drawRectangle(x + 1 + (self.x * hud.x_scale), y + (self.y * 8), 6, 4, self.color, 1)
 	--shadow
-	emu.drawLine(x + (self.x * x_scale) + 1, y + (self.y * 8) + 3, x + (self.x * x_scale) + 6, y + (self.y * 8) + 3, 0xAF000000)
-	emu.drawLine(x + (self.x * x_scale) + 1, y + (self.y * 8) + 0, x + (self.x * x_scale) + 6, y + (self.y * 8) + 0, 0xAF000000)
+	emu.drawLine(x + (self.x * hud.x_scale) + 1, y + (self.y * 8) + 3, x + (self.x * hud.x_scale) + 6, y + (self.y * 8) + 3, 0xAF000000)
+	emu.drawLine(x + (self.x * hud.x_scale) + 1, y + (self.y * 8) + 0, x + (self.x * hud.x_scale) + 6, y + (self.y * 8) + 0, 0xAF000000)
 end
 
 function StartRobotBlock()
@@ -637,7 +641,7 @@ function StartRobotBlock()
 
 	objects.blockred = {
 		x = 0,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -652,7 +656,7 @@ function StartRobotBlock()
 
 	objects.blockwhite = {
 		x = 1,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -667,7 +671,7 @@ function StartRobotBlock()
 
 	objects.blockblue = {
 		x = 2,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -682,7 +686,7 @@ function StartRobotBlock()
 
 	objects.blockyellow = {
 		x = 3,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -697,7 +701,7 @@ function StartRobotBlock()
 
 	objects.blockgreen = {
 		x = 4,
-		y = y_base - 1,
+		y = phys.y_base - 1,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -714,7 +718,7 @@ function StartRobotBlock()
 
 	objects.holder1 = {
 		x = 0,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -728,7 +732,7 @@ function StartRobotBlock()
 
 	objects.holder2 = {
 		x = 1,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -742,7 +746,7 @@ function StartRobotBlock()
 
 	objects.holder3 = {
 		x = 2,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -756,7 +760,7 @@ function StartRobotBlock()
 
 	objects.holder4 = {
 		x = 3,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,
@@ -770,7 +774,7 @@ function StartRobotBlock()
 
 	objects.holder5 = {
 		x = 4,
-		y = y_base,
+		y = phys.y_base,
 		gravity = 0,
 		grabbed = 0,
 		falling = 0,

c0324cde8f559edd9f661d8a77619fd720121e36
LuigiBlood <luigiblood@gmail.com>
2026-07-01 04:02:56 +0200

    [SPCHelper] Change imGUI + add manual end/jump detection for Star Fox 2

--- SNES/Mesen/SPCHelper.lua
+++ SNES/Mesen/SPCHelper.lua
@@ -11,18 +11,31 @@ end
 
 state = emu.getState()
 emu.selectDrawSurface(emu.drawSurface.scriptHud, 1)
-bgColor = 0x302060FF
-bgColorHover = 0x305090FF
-bgColorClick = 0x301010FF
-fgColor = 0x30FF4040
-txColor = 0xFFFFFF
+
 preMouseState = emu.getMouseState()
 curMouseState = emu.getMouseState()
 values = {0, 0, 0, 0}
+
+--Style
+--Normal, Hover, Click
+bgPalette = {0x30C0C0C0, 0x30E0E0E0, 0x30C0C0C0, 0x302060FF, 0x30FFFFFF, 0x30000000}
+--Top Left Shade
+fgPalette1 = {0x30F0F0F0, 0x30F0F0F0, 0x30505050, 0x30FF4040, 0x30505050, 0xFF000000}
+--Bottom Right Shade
+fgPalette2 = {0x30505050, 0x30505050, 0x30F0F0F0, 0x30FF4040, 0x30505050, 0xFF000000}
+txPalette = {0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00FFFFFF}
+
+--SPC Helper States
+config = {
+	starfox2 = false
+}
 flagSaveNextNote = false
 flagSaveTimerStart = false
 timerStart = false
 timer = 0
+jump = 0
+callback_jumpcheck = nil
+callback_endcheck = nil
 
 --Util
 function checkMouseBox(x, y, w, h)
@@ -31,6 +44,10 @@ end
 
 function checkMouseLeftClick()
 	return (preMouseState.left == false and curMouseState.left == true)
+end
+
+function checkMouseLeftHold()
+	return (curMouseState.left == true)
 end
 
 function fileExists(name)
@@ -44,82 +61,131 @@ function fileExists(name)
 end
 
 --Immediate GUI
-function arrowUpUI(x, y)
-	local testColor = 0
+function rectangleUI(x, y, w, h, p)
+	p = p or 1
+	local curBgColor = bgPalette[p]
+	local curFgColor1 = fgPalette1[p]
+	local curFgColor2 = fgPalette2[p]
+	
+	emu.drawRectangle(x, y, w, h, curBgColor, true)
+	emu.drawRectangle(x, y, w, 1, curFgColor1, false)
+    emu.drawRectangle(x, y+1, 1, h-2, curFgColor1, false)
+    
+    emu.drawRectangle(x, y+h-1, w, 1, curFgColor2, false)
+    emu.drawRectangle(x+w-1, y+1, 1, h-1, curFgColor2, false)
+end
+
+function textUI(x, y, txt, p)
+	p = p or 1
+	local curTxColor = txPalette[p]
+	
+	emu.drawString(x, y, txt, curTxColor, 0xFF000000)
+end
+
+function arrowUI(x, y, t, p)
+	p = p or 1
+	local curColorType = p + 0
 	local click = false
-	testColor = bgColor
+	local held = 0
 	if (checkMouseBox(x, y, 11, 7)) then
-		testColor = bgColorHover
+		curColorType = p + 1
+		if checkMouseLeftHold() == true then
+			curColorType = p + 2
+			held = 1
+		end
 		if checkMouseLeftClick() == true then
-			testColor = bgColorClick
 			click = true
 		end
 	end
 	
-	emu.drawRectangle(x, y, 11, 7, testColor, true)
-    emu.drawRectangle(x, y, 11, 7, fgColor, false)
-    emu.drawRectangle(x+5, y+2, 1, 1, txColor, false)
-    emu.drawRectangle(x+4, y+3, 3, 1, txColor, false)
-    emu.drawRectangle(x+3, y+4, 5, 1, txColor, false)
+	local curTxColor = txPalette[curColorType]
+	
+	rectangleUI(x+held, y+held, 11, 7, curColorType)
+	if t then
+	    emu.drawRectangle(x+5+held, y+2+held, 1, 1, curTxColor, false)
+	    emu.drawRectangle(x+4+held, y+3+held, 3, 1, curTxColor, false)
+	    emu.drawRectangle(x+3+held, y+4+held, 5, 1, curTxColor, false)
+    else
+	    emu.drawRectangle(x+5+held, y+4+held, 1, 1, curTxColor, false)
+	    emu.drawRectangle(x+4+held, y+3+held, 3, 1, curTxColor, false)
+	    emu.drawRectangle(x+3+held, y+2+held, 5, 1, curTxColor, false)
+    end
     
     return click
 end
 
-function arrowDnUI(x, y)
-	local testColor = 0
-	local click = false
-	testColor = bgColor
-	if (checkMouseBox(x, y, 11, 7)) then
-		testColor = bgColorHover
-		if checkMouseLeftClick() == true then
-			testColor = bgColorClick
-			click = true
-		end
-	end
-	
-	emu.drawRectangle(x, y, 11, 7, testColor, true)
-    emu.drawRectangle(x, y, 11, 7, fgColor, false)
-    emu.drawRectangle(x+5, y+4, 1, 1, txColor, false)
-    emu.drawRectangle(x+4, y+3, 3, 1, txColor, false)
-    emu.drawRectangle(x+3, y+2, 5, 1, txColor, false)
-    
-    return click
+function arrowUpUI(x, y, p)
+	p = p or 1
+	return arrowUI(x, y, true, p)
 end
 
-function textBoxUI(x, y, txt, color)
-	color = color or bgColor
-	local widthString = emu.measureString(txt)
-	
-	emu.drawRectangle(x, y, 4+widthString.width, 4+widthString.height, color, true)
-    emu.drawRectangle(x, y, 4+widthString.width, 4+widthString.height, fgColor, false)
-    emu.drawString(x+2, y+3, txt, txColor, 0xFF000000)
+function arrowDnUI(x, y, p)
+	p = p or 1
+	return arrowUI(x, y, false, p)
 end
 
-function buttonUI(x, y, txt)
+function textBoxUI(x, y, txt, p)
+	p = p or 1
 	local widthString = emu.measureString(txt)
 	
-	local testColor = 0
+	rectangleUI(x, y, 4+widthString.width, 4+widthString.height, p)
+    textUI(x+2, y+3, txt, p)
+end
+
+function buttonUI(x, y, txt, p)
+	p = p or 1
+	
+	local widthString = emu.measureString(txt)
+	
+	local curColorType = p + 0
 	local click = false
+	local held = 0
 	--first Digit Up
-	testColor = bgColor
 	if (checkMouseBox(x, y, 4+widthString.width, 4+widthString.height)) then
-		testColor = bgColorHover
+		curColorType = p + 1
+		if checkMouseLeftHold() == true then
+			curColorType = p + 2
+			held = 1
+		end
 		if checkMouseLeftClick() == true then
-			testColor = bgColorClick
+			click = true
+		end
+	end
+
+	textBoxUI(x+held, y+held, txt, curColorType)
+    
+    return click
+end
+
+function checkBoxUI(x, y, txt, var, p)
+	p = p or 1
+	local widthString = emu.measureString(txt)
+	
+	local curColorType = p + 0
+	local click = false
+	
+	if (checkMouseBox(x, y, 12+widthString.width, widthString.height)) then
+		if checkMouseLeftClick() == true then
 			click = true
 		end
 	end
 	
-	textBoxUI(x, y, txt, testColor)
-    
-    return click
+	if click then
+		config[var] = not config[var]
+	end
+	
+	local check = config[var]
+
+	emu.drawRectangle(x, y, 7, 7, 0xFFFFFF, check)
+	textUI(x+12, y, txt, curColorType)
+	
+	return click
 end
 
 function numberBoxUI(x, y, v)
-	emu.drawRectangle(x, y, 22, 11, bgColor, true)
-    emu.drawRectangle(x, y, 22, 11, fgColor, false)
-    emu.drawString(x+3, y+2, string.format('%1X', v >> 4), txColor, 0xFF000000)
-    emu.drawString(x+14, y+2, string.format('%1X', v & 0x0F), txColor, 0xFF000000)
+	rectangleUI(x,y,22,11)
+    textUI(x+3, y+2, string.format('%1X', v >> 4))
+    textUI(x+14, y+2, string.format('%1X', v & 0x0F))
 end
 
 function numberModUI(x, y, val)
@@ -194,7 +260,27 @@ function setTimeSPC(fade)
 	timerStart = false
 end
 
-function nextNoteCheck(address, value)
+function callbackEndDetection()
+	--if song just ends
+	if timerStart then
+		emu.displayMessage("SPC", "Song has ended.")
+		setTimeSPC(0)
+	end
+end
+
+function callbackJumpDetection()
+	--if song jumps/loops, keep track of loops until save
+	if timerStart then
+		jump = jump + 1
+		emu.displayMessage("SPC", "Song has jumped " .. jump .. " time(s).")
+		if jump == 2 then
+			setTimeSPC(5000)
+			jump = 0
+		end
+	end
+end
+
+function callbackNextNoteCheck(address, value)
 	--thanks Nova
 	local dspreg = emu.read(0xF2, emu.memType.spcMemory)
 	local dspdata = emu.read(dspreg, emu.memType.spcDspRegisters)
@@ -211,7 +297,7 @@ function nextNoteCheck(address, value)
 	end
 end
 
-emu.addMemoryCallback(nextNoteCheck, emu.callbackType.write, 0xF3, 0xF3, emu.cpuType.spc, emu.memType.spcMemory)
+emu.addMemoryCallback(callbackNextNoteCheck, emu.callbackType.write, 0xF3, 0xF3, emu.cpuType.spc, emu.memType.spcMemory)
 
 function manageScreen()
 	if timerStart then
@@ -222,8 +308,10 @@ function manageScreen()
 	preMouseState = curMouseState
 	curMouseState = emu.getMouseState()
 	
-	local x = 0
-	local y = 0
+	local x = 8
+	local y = 8
+	
+	textUI(x, y, "SPC Helper", 4)
     
     --emu.drawRectangle(8, 8, 128, 24, bgColor, true, 1)
     --emu.drawRectangle(8, 8, 128, 24, fgColor, false, 1)
@@ -248,40 +336,60 @@ function manageScreen()
     	emu.write(0x2143, values[4], emu.memType.snesMemory)
     end
     
-    x = 8; y = 35
+    x = 8; y = 29
     if buttonUI(x,y,"Save SPC") then
     	makeSPC()
     end
-    if buttonUI(x,y+18,"Save SPC (Next Note)") then
+    if buttonUI(x,y+16,"Save SPC (Next Note)") then
     	flagSaveNextNote = true
     end
-    if buttonUI(x,y+35,"Save SPC (Next Note)\n+ Start Timer") then
+    if buttonUI(x,y+32,"Save SPC (Next Note)\n+ Start Timer") then
     	flagSaveNextNote = true
     	flagSaveTimerStart = true
     end
+    if flagSaveNextNote then
+    	textUI(x, y+60, "Pending Next Note...", 4)
+    end
+    x = 8; y = 100
+    textBoxUI(x, y,string.format("Time: %d:%02d", math.floor(timer / 60 / 60), math.floor(timer / 60 % 60)), 5)
     if timerStart then
-    	textBoxUI(x, y+64,string.format("Time: %d:%02d", math.floor(timer / 60 / 60), math.floor(timer / 60 % 60)), 0x50A02020)
-    	if buttonUI(x,y+80,"Stop & Save Time to SPC (Fade)") then
+    	if buttonUI(x,y+15,"Cancel") then
+	    	timerStart = false
+	    end
+    	if buttonUI(x+45,y+15,"Stop & Save\n(5s Fade)") then
 	    	setTimeSPC(5000)
 	    end
-	    if buttonUI(x,y+100,"Stop & Save Time to SPC (No Fade)") then
+	    if buttonUI(x+120,y+15,"Stop & Save\n(No Fade)") then
 	    	setTimeSPC(0)
 	    end
     end
     
     x = 90; y = 8
-    emu.drawRectangle(x, y, 158, 22, bgColor, true)
-    emu.drawRectangle(x, y, 158, 22, fgColor, false)
-    emu.drawString(x+2+2, y+2, "CPU -> APU:", txColor, 0xFF000000)
+    rectangleUI(x, y, 158, 22)
+    textUI(x+2+2, y+2, "CPU -> APU:")
     numberBoxUI(x+70+(22*0), y, state["spc.cpuRegs[0]"])
     numberBoxUI(x+70+(22*1), y, state["spc.cpuRegs[1]"])
     numberBoxUI(x+70+(22*2), y, state["spc.cpuRegs[2]"])
     numberBoxUI(x+70+(22*3), y, state["spc.cpuRegs[3]"])
-    emu.drawString(x+2+2, y+2+11, "CPU <- APU:", txColor, 0xFF000000)
+    textUI(x+2+2, y+2+11, "CPU <- APU:")
     numberBoxUI(x+70+(22*0), y+11, state["spc.outputReg[0]"])
     numberBoxUI(x+70+(22*1), y+11, state["spc.outputReg[1]"])
     numberBoxUI(x+70+(22*2), y+11, state["spc.outputReg[2]"])
     numberBoxUI(x+70+(22*3), y+11, state["spc.outputReg[3]"])
+    
+    x = 8; y = 150
+    rectangleUI(x, y, 200, 11*2, 6)
+    textUI(x+2, y+2+(11*0), "Configuration:", 6)
+    if checkBoxUI(x+2, y+2+(11*1), "Star Fox 2 Auto Timer", "starfox2", 6) then
+    	--Star Fox 2 Final Specific
+    	if config.starfox2 then
+    		callback_endcheck = emu.addMemoryCallback(callbackEndDetection, emu.callbackType.exec, 0x0888, 0x0888, emu.cpuType.spc, emu.memType.spcMemory)
+			callback_jumpcheck = emu.addMemoryCallback(callbackJumpDetection, emu.callbackType.exec, 0x088B, 0x088B, emu.cpuType.spc, emu.memType.spcMemory)
+		else
+			emu.removeMemoryCallback(callback_endcheck, emu.callbackType.exec, 0x0888, 0x0888, emu.cpuType.spc, emu.memType.spcMemory)
+			emu.removeMemoryCallback(callback_jumpcheck, emu.callbackType.exec, 0x088B, 0x088B, emu.cpuType.spc, emu.memType.spcMemory)
+    	end
+    end
     
     emu.drawRectangle(curMouseState.x, curMouseState.y, 4, 1, 0xFFFFFF)
 	emu.drawRectangle(curMouseState.x, curMouseState.y, 1, 4, 0xFFFFFF)
